73a29b4467d5fd11b247d1eec6866e8beb526d0f
KonradBA_dev <qndziulek@gmail.com>
2025-04-02 16:50:19 +0200

    Update section skill - add skill, media q, add class content
    In the Skills section, I added new HTML elements and new animation, extended the styling with additional CSS rules, and implemented media queries to make it fully responsive. I also included some simple animations to enhance the user experience.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function isSectionInView(section, offset = 0) {
     const windowHeight = window.innerHeight;
 
     // Sprawdzenie, czy sekcja jest częściowo widoczna na ekranie
+    // Sprawdzamy, czy sekcja jest częściowo widoczna na ekranie
     return sectionTop <= windowHeight && sectionBottom >= 0;
 }
 
@@ -148,10 +149,13 @@ function handleScroll() {
     // Dodanie klasy 'visible' do sekcji w widoku
     const sections = document.querySelectorAll('.section');
     sections.forEach(section => {
+        const content = section.querySelector('.content');
         if (isSectionInView(section)) { // Sprawdzamy, czy sekcja jest w widoku
             section.classList.add('visible');
+            content.classList.add('visible'); // Pokazujemy zawartość sekcji
         } else {
             section.classList.remove('visible');
+            content.classList.remove('visible'); // Ukrywamy zawartość sekcji
         }
     });
 
